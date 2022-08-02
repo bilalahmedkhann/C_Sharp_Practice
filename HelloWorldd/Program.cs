43e5f0581c78b3b8a288c826ab3f5acd2079e51f
bilalahmedkhann <bilal.ahmed98@gmail.com>
2022-08-03 02:22:15 +0530

    Practice for C#

---
+++ HelloWorldd/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HelloWorldd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int a = 560;
+            int b = 72;
+            int c = (a / b);
+            int d = (a % b);
+            Console.WriteLine("Division of two numbers is "+c);
+            Console.WriteLine("Modulo of two numbers is "+d);
+            Console.WriteLine("Addition of two numbers is "+(a + b));
+            Console.WriteLine("I am learning C# and it is awesome");
+
+        }
+    }
+}
