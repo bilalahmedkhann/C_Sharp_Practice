74a0bf2277e359b7c7f1c59129459b317209add7
bilalahmedkhann <bilal.ahmed98@gmail.com>
2022-08-03 02:40:37 +0530

    Practice for C#

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 56;
+            int b = 8;
+            int c = a + b;
+            int d = a - b;
+            Console.WriteLine(a + b + c + d);
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your name: ");
+            string name = Console.ReadLine();
+
+
+            Console.WriteLine("What is your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Hello "+name+", How are you? and your age is "+ age);
+
+         }
+    }
+}
